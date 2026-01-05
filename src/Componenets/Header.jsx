55f498046a289f00e8a4027330df2dcdb18c06e9
nor-abdiwali <nnuurcabdiwali@gmail.com>
2026-01-05 10:27:38 +0300

    badalay

--- src/Componenets/Header.jsx
+++ src/Componenets/Header.jsx
@@ -3,19 +3,19 @@ import { FaWifi } from "react-icons/fa6";
 function Header() {
     return (
         <>
-        <div className="flex gap-40 bg-white shadow-2xs h-15 py-2 px-5 text-2xl">
+        <div className="flex gap-40 bg-gray-100 justify-between font-sans shadow-2xl h-20 py-6 px-5">
             <div className="flex gap-5">
-                <p><FaWifi className="mt-2 text-3xl text-blue-500" /></p>
-            <h1 className="font-bold text-3xl">InternetPay</h1>
+                <p><FaWifi className="mt-2 text-4xl text-blue-500 mt-[-1px]" /></p>
+            <h1 className="font-bold text-2xl">InternetPay</h1>
             </div>
-            <ul className="flex gap-10">
+            <ul className="flex gap-10 pt-2">
 
                 <Link to = "/"><li>Home</li></Link>
                 <Link to = "/about"><li>About</li></Link>
                 <Link to = "/services"><li>Services</li></Link>
                 <div className="flex gap-10 ml-59 text-xl">
-                <button className="bg-white shadow-2xs text-center w-25 h-10 border-1 rounded-2xl"><Link to = "/login"><li>Login</li></Link></button>
-                <button className="bg-black text-white shadow-2xs text-center w-35 h-10 border-1 rounded-2xl"><Link to = "/register"><li>Register</li></Link></button>
+                <button className="bg-white text-[18px] shadow-2xs text-center w-25 h-12 border-1 rounded-2xl mt-[-10px]"><Link to = "/login"><li>Login</li></Link></button>
+                <button className="bg-black text-[18px] text-white shadow-2xs text-center w-30 h-12 border-1 rounded-2xl mt-[-10px]"><Link to = "/register"><li>Register</li></Link></button>
 
                 </div>
                
